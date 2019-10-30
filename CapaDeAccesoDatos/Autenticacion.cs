af7a5de8586796d364d2e4a196faeef94f51128e
ismael08-jpg <56751050+ismael08-jpg@users.noreply.github.com>
2019-10-30 10:31:28 -0600

    Conexion y Referencias entre clases

---
+++ CapaDeAccesoDatos/Autenticacion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeAccesoDatos
+{
+    /* public class Autenticacion
+     {
+         public string tipousuario(string usuario)
+         {
+             SqlConnection con = new SqlConnection(conexion.cadenaconexion());
+             con.Open();
+             string cadena = "";
+             DataTable dt = new DataTable();
+             SqlDataAdapter sda = new SqlDataAdapter();
+             sda = new SqlDataAdapter("select * from usuario where usuario= '" + usuario + "'", con);
+             sda.Fill(dt);
+             if (dt.Rows.Count == 1)
+             {
+                 if (dt.Rows[0][3].ToString() == "admin")
+                 {
+                     cadena = "admin";
+                     con.Close();
+                     return cadena;
+                 }
+                 else if (dt.Rows[0][3].ToString() == "prof")
+                 {
+                     cadena = "prof";
+                     con.Close();
+                     return cadena;
+                 }
+                 else if (dt.Rows[0][3].ToString() == "alum")
+                 {
+                     cadena = "alum";
+                     con.Close();
+                     return cadena;
+                 }
+             }
+             con.Close();
+             return cadena;
+         }
+
+         public int verificar(string usuario, string contrasena)
+         {
+             int resultado = -1;
+
+             SqlConnection con = new SqlConnection(conexion.cadenaconexion());
+             try
+             {
+                 con.Open();
+                 SqlCommand cmd = new SqlCommand(string.Format("select * from usuario where usuario='" + usuario + "'" + " and " + "contrasena='" + contrasena + "'"), con);
+                 SqlDataReader dr = cmd.ExecuteReader();
+                 while (dr.Read())
+                 {
+
+                     resultado = 1;
+                 }
+                 if (resultado == -1)
+                 {
+                     MessageBox.Show("Lo sentimos el Usuario o contraseña son incorrectos");
+                 }
+                 con.Close();
+                 return resultado;
+             }
+             catch (SqlException error)
+             {
+                 MessageBox.Show(error.Message);
+                 con.Close();
+                 return -1;
+             }
+
+         }
+     }*/
+}
---
+++ CapaDeAccesoDatos/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeAccesoDatos
+{
+    class Conexion
+    {
+        public SqlConnection conex = new SqlConnection("Data source =DESKTOP-4CJHEF7\\SQLEXPRESS; Initial catalog = ComidaRapida; Integrated Security=True");
+
+        public void abrir_conexion()
+        {
+            if (conex.State == ConnectionState.Closed)
+                conex.Open();
+        }
+
+        public void cerrar_conexion()
+        {
+            if (conex.State == ConnectionState.Open)
+                conex.Close();
+        }
+    }
+}
