05b3a9b390b1adb6b16d1583a1266bec38205619
luisma345 <luisbonillla345@gmail.com>
2019-11-02 23:06:14 -0600

    Cajero empezando

---
+++ CapaDeAccesoDatos/DestalleOrden.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeAccesoDatos
+{
+    public class DestalleOrden
+    {
+        private int noOrden;
+        private int idMenu;
+        private string extra;
+        private int cantidad;
+        private double precioUnidad;
+        private double descuento;
+        private int idCategoria;
+
+        public int NoOrden
+        {
+            get
+            {
+                return noOrden;
+            }
+
+            set
+            {
+                noOrden = value;
+            }
+        }
+
+        public int IdMenu
+        {
+            get
+            {
+                return idMenu;
+            }
+
+            set
+            {
+                idMenu = value;
+            }
+        }
+
+        public string Extra
+        {
+            get
+            {
+                return extra;
+            }
+
+            set
+            {
+                extra = value;
+            }
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+
+            set
+            {
+                cantidad = value;
+            }
+        }
+
+        public double PrecioUnidad
+        {
+            get
+            {
+                return precioUnidad;
+            }
+
+            set
+            {
+                precioUnidad = value;
+            }
+        }
+
+        public double Descuento
+        {
+            get
+            {
+                return descuento;
+            }
+
+            set
+            {
+                descuento = value;
+            }
+        }
+
+        public int IdCategoria
+        {
+            get
+            {
+                return idCategoria;
+            }
+
+            set
+            {
+                idCategoria = value;
+            }
+        }
+
+        public DestalleOrden() {
+
+        }
+
+    }
+}
---
+++ CapaDeAccesoDatos/MantenimientoDetalleOrden.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeAccesoDatos
+{
+    class MantenimientoDetalleOrden
+    {
+        string sql;
+        Conexion conn = new Conexion();
+        SqlCommand cmd;
+        string error;
+        int respuesta = 0;
+
+        //Mostrar Informacion DetallePedido
+        public DataTable mostrarDetallePedido(string tabla){
+            DataTable datos = new DataTable();
+            SqlDataAdapter adap;
+            this.sql = "select * from " + tabla;
+            
+            try {
+                conn.abrir_conexion();
+                adap = new SqlDataAdapter(this.sql, conn.conex);
+                adap.Fill(datos);
+            }
+            catch(Exception e) {
+                error = "Error " + e.ToString();
+            } 
+            finally {
+                conn.cerrar_conexion();
+            }
+            return datos;
+                
+        }
+    }
+}
