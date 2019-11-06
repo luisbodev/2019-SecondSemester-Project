8cadc8bf413145f59796f7e3460c44ef0260ee4b
ismael08-jpg <56751050+ismael08-jpg@users.noreply.github.com>
2019-11-05 22:21:22 -0600

    Cambios

---
+++ SistemaComidaRapida/FrmReportePedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaComidaRapida
+{
+    public partial class FrmReportePedido : Form
+    {
+        public FrmReportePedido()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            string date = dateTime.Text;
+            MessageBox.Show("Es: " + date);
+             
+        }
+    }
+}
