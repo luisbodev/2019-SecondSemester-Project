66d72d6bb40178fe0a74e799838b7efc14279e06
luisma345 <luisbonillla345@gmail.com>
2019-11-08 09:25:01 -0600

    arreglado

--- SistemaComidaRapida/DatosOrdenes.cs
+++ SistemaComidaRapida/DatosOrdenes.cs
@@ -20,17 +20,19 @@ namespace SistemaComidaRapida
         TransacOrden obje = new TransacOrden();
         TransacDetalleOrden detaOrden = new TransacDetalleOrden();
         string respuesta = "";
-           
+
         public frmDatosOrdenes()
         {
             InitializeComponent();
         }
 
-        void llenar() {
+        void llenar()
+        {
             dgvOrden.DataSource = obje.mostrar_Orden();
         }
-        
-        void llenardaosorden() {
+
+        void llenardaosorden()
+        {
             dgvDetalleOrden.DataSource = detaOrden.mostrar_DetalleOrdenCaje(dgvOrden.CurrentRow.Cells[0].Value.ToString());
         }
         private void frmDatosOrdenes_Load(object sender, EventArgs e)
@@ -38,7 +40,7 @@ namespace SistemaComidaRapida
             llenar();
         }
 
-      
+
 
         private void dgvOrden_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
@@ -57,53 +59,63 @@ namespace SistemaComidaRapida
             double total;
 
 
-            if (!(dgvDetalleOrden.CurrentRow.Cells[0].Value.ToString() == "")){
+            if (!(dgvDetalleOrden.CurrentRow.Cells[0].Value.ToString() == ""))
+            {
                 txtIdMenu.Text = dgvDetalleOrden.CurrentRow.Cells[0].Value.ToString();
                 txtNombreMenu.Text = dgvDetalleOrden.CurrentRow.Cells[1].Value.ToString();
                 txtExtra.Text = dgvDetalleOrden.CurrentRow.Cells[2].Value.ToString();
                 txtCantidad.Text = dgvDetalleOrden.CurrentRow.Cells[3].Value.ToString();
                 txtPrecioUni.Text = dgvDetalleOrden.CurrentRow.Cells[4].Value.ToString();
 
-                total = double.Parse(txtPrecioUni.Text) * int.Parse(txtCantidad.Text);
-                txtTotalMe.Text = total.ToString();
+                total = double.Parse(dgvDetalleOrden.CurrentRow.Cells[4].Value.ToString()) * int.Parse(txtCantidad.Text = dgvDetalleOrden.CurrentRow.Cells[3].Value.ToString());
+                txtTotalMe.Text = total.ToString();///-----------------------------------------------
 
 
                 btnModificarDeta.Enabled = true;
                 btnEliminarDeta.Enabled = true;
             }
-            
+
         }
 
         private void txtIdMenu_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void btnModificarDeta_Click(object sender, EventArgs e)
         {
             double totalFinal;
-            if(int.Parse(txtCantidad.Text)>cantidad) {
+
+            if (int.Parse(txtCantidad.Text) > cantidad)
+            {
                 totalFinal = double.Parse(txtTotal.Text) + double.Parse(txtTotalMe.Text);
             }
-            else {
+            else
+            {
                 totalFinal = double.Parse(txtTotal.Text) - double.Parse(txtTotalMe.Text);
             }
+
+            //totalFinal = double.Parse(txtPrecioUni.Text) * int.Parse(txtCantidad.Text);
+
             obje.total_FinalOrden(totalFinal.ToString(), txtNoOrden.Text);
+
             detaOrden.actualizar_Det(totalDeta.ToString(), txtIdMenu.Text, txtNoOrden.Text);
+
             respuesta = detaOrden.modificar_DetalleOrden(txtNoOrden.Text, txtIdMenu.Text, txtExtra.Text, txtCantidad.Value.ToString(), txtPrecioUni.Text, txtTotalMe.Text);
-            
 
 
 
-            if (respuesta=="1") {
+
+            if (respuesta == "1")
+            {
                 MessageBox.Show("Registro Modificado Correctamente");
                 btnModificarDeta.Enabled = false;
                 btnEliminarDeta.Enabled = false;
-                
+
                 llenardaosorden();
                 llenar();
                 txtTotal.Text = dgvOrden.CurrentRow.Cells[4].Value.ToString();
-               
+
                 txtIdMenu.Clear();
                 txtNombreMenu.Clear();
                 txtExtra.Clear();
@@ -111,38 +123,44 @@ namespace SistemaComidaRapida
                 txtPrecioUni.Clear();
                 txtTotalMe.Clear();
             }
-            else {
+            else
+            {
                 MessageBox.Show("Registro no modificado" + respuesta);
             }
         }
 
-        
+        int cantidadDetalle;
+
         private void txtCantidad_ValueChanged(object sender, EventArgs e)
         {
             
-            if (!(dgvDetalleOrden.CurrentRow.Cells[0].Value.ToString() == ""))
-            {
-                cantidad = int.Parse(txtCantidad.Text);
-                totalDeta = double.Parse(dgvDetalleOrden.CurrentRow.Cells[3].Value.ToString()) * cantidad;
-                txtTotalMe.Text = totalDeta.ToString();
-            }
+        }
+
+        private void btnCambiar_Click(object sender, EventArgs e)
+        {
+            cantidadDetalle = int.Parse(txtCantidad.Text);
+            totalDeta = double.Parse(txtPrecioUni.Text) * cantidadDetalle;///--------------<><<<<>>>>><<<<>
+            txtTotalMe.Text = totalDeta.ToString();
+            
 
         }
 
+        
         private void btnEliminarDeta_Click(object sender, EventArgs e)
         {
             double totalFinal;
             if (MessageBox.Show("¿Está seguro de Eliminar el registro?", "Eliminar Registro", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
-                
+
                 respuesta = detaOrden.eliminar_Detalle(txtNoOrden.Text, txtIdMenu.Text);
                 btnModificarDeta.Enabled = false;
                 btnEliminarDeta.Enabled = false;
 
-                
+
                 totalFinal = double.Parse(txtTotal.Text) - double.Parse(txtTotalMe.Text);
 
                 obje.total_FinalOrden(totalFinal.ToString(), txtNoOrden.Text);
+
                 txtIdMenu.Clear();
                 txtNombreMenu.Clear();
                 txtExtra.Clear();
@@ -151,7 +169,7 @@ namespace SistemaComidaRapida
                 txtTotalMe.Clear();
                 llenardaosorden();
                 llenar();
-                txtTotal.Text= dgvOrden.CurrentRow.Cells[4].Value.ToString();
+                txtTotal.Text = dgvOrden.CurrentRow.Cells[4].Value.ToString();
                 if (respuesta == "1")
                     MessageBox.Show("Registro Eliminado Correctamente");
                 else
@@ -159,5 +177,5 @@ namespace SistemaComidaRapida
             }
         }
     }
-    
+
 }
